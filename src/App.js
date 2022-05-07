b5a8047778fc2fdf6e856711fe8267ee793deca3
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-05-07 17:47:41 -0300

    header left - search - Header Component

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ import './App.css';
 const App = () => {
     return (
         <div className='app'>
-
-            <h1>Lets build a LinkedIn CLONE</h1>
-
+            
             {/* Header */}
             <Header />
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import SearchIcon from '@mui/icons-material/Search';
+
 import './Header.css';
 
 
@@ -8,15 +10,21 @@ const Header = () => {
     return (
         <div className='header'>
 
-            <h1>This is the header</h1>
-
             <div className="header__left">
-                <img src='' alt=''/>
+                <img 
+                    alt=''
+                    // https://www.flaticon.com/premium-icon/linkedin_3536505?term=linkedin&page=1&position=2&page=1&position=2&related_id=3536505&origin=search
+                    src='https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1651955409~hmac=f87afc072b3171a1f1a67378a6eb22b8' 
+                />
+            </div>
+
+            <div className="header__search">
+                <SearchIcon />
+                <input type='text'/>
             </div>
 
             <div className="header__right">
-                {/* SearchIcon */}
-                <input type='text'/>
+                
             </div>
 
         </div>
