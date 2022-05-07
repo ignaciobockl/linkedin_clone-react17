0b0c0cce6735c04fbfb4a146c448f83014e13fe8
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-05-07 16:53:22 -0300

    Header component in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 import './App.css';
 
 
 
 const App = () => {
     return (
-      <div className='app'>
-          <h1>Lets build a LinkedIn CLONE</h1>
-      </div>
+        <div className='app'>
+
+            <h1>Lets build a LinkedIn CLONE</h1>
+
+            {/* Header */}
+            <Header />
+
+            {/* App Body */}
+                {/* Sidebar */}
+                {/* Feed */}
+                {/* Widgets */}
+
+        </div>
     );
 }
 
