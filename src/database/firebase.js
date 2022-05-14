a33aa4039d142b868b7b30fdfd4f9154dacd5698
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-05-13 22:19:22 -0300

    firebase config version 8

---
+++ src/database/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+const apiKey = process.env.REACT_APP_FIREBASE_APIKEY;
+const authDomain = process.env.REACT_APP_FIREBASE_AUTHDOMAIN;
+const projectId = process.env.REACT_APP_FIREBASE_PROJECTID;
+const storageBucket = process.env.REACT_APP_FIREBASE_STORAGEBUCKET;
+const messagingSenderId = process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID;
+const appId = process.env.REACT_APP_FIREBASE_APPID;
+
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
